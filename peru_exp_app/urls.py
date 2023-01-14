506128d6e6ebd4e504ba2c634c5ddaae31eda7c2
IvetteMcDermott <ivettedemcdermott@gmail.com>
2023-01-14 20:43:29 +0000

    Add PlaceInformation, detail view of information

--- peru_exp_app/urls.py
+++ peru_exp_app/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.landing_page, name='home'),
     path('coast/', views.Coast.as_view(), name='coast_places_display'),
     path('andes/', views.Andes.as_view(), name='andes_places_display'),
-    path('jungle/', views.Jungle.as_view(), name='jungle_places_display')
+    path('jungle/', views.Jungle.as_view(), name='jungle_places_display'),
+    path('<slug:slug>/', views.PlaceInformation.as_view(), name='place_information')
 ]
--- peru_exp_app/views.py
+++ peru_exp_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
+from django.http import HttpResponseRedirect
+
 from django.http import HttpResponse
 from .models import PlacesList
 
@@ -40,12 +42,25 @@ class Andes(View):
 
 class Jungle(View):
     def get(self, request, *args, **kwargs):
-            model = PlacesList
-            data_filtered = PlacesList.objects.filter(region='Jungle')
-            template = "region_places.html"
-            paginate_by = 6
-            context = {
-                'context': data_filtered
-            }
+        model = PlacesList
+        data_filtered = PlacesList.objects.filter(region='Jungle')
+        template = "region_places.html"
+        paginate_by = 6
+        context = {
+            'context': data_filtered
+        }
 
-            return render(request, template, context)
+        return render(request, template, context)
+
+
+class PlaceInformation(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        place = PlacesList.objects.all()
+        place_data = get_object_or_404(place, slug=slug)
+        template = 'place_information.html'
+        context = {
+            'context': place_data
+        }
+
+        return render(request, template, context)
