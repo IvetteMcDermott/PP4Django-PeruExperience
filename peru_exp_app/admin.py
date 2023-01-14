b95bdda5f26dab02810e43c07c69cad374f743d6
IvetteMcDermott <ivettedemcdermott@gmail.com>
2023-01-14 15:49:12 +0000

    Make migrations.Create Models in DB

--- peru_exp_app/admin.py
+++ peru_exp_app/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from .models import PlacesList, Comment, UserProfile
 
 # Register your models here.
 
@@ -7,19 +8,19 @@ from django.contrib import admin
 class PlacesItems(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('place',)}
     list_filter = ('region', 'type_location')
-    search_fields = ('region', 'type_location', 'place')
+    search_fields = ['region', 'type_location', 'place']
     list_display = ('place', 'type_location', 'location', 'info', 'region', 'altitude', 'author', 'date_created', 'date_updated')
 
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('place', 'body', 'author', 'date_created', 'date_updated')
+    list_display = ('place', 'body', 'date_created', 'date_updated')
     list_filter = ('author', 'place')
-    search_fields = ('author', 'place')
+    search_fields = ['author', 'place']
 
 
 @admin.register(UserProfile)
 class UserProfile(admin.ModelAdmin):
-    list_display = ('user', 'user_name', 'interests')
+    list_display = ['user', 'user_name']
     list_filter = ('user_name', 'interests')
-    search_fields = ('user_name')
+    search_fields = ['user_name']
