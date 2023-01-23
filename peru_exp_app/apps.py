d33d3dcdd237a029c3a9b9cd247e79f2f0dc8b12
IvetteMcDermott <ivettedemcdermott@gmail.com>
2023-01-23 14:13:30 +0000

    Create signals.py and add code for UserProfiles

--- peru_exp_app/apps.py
+++ peru_exp_app/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class PeruExpAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'peru_exp_app'
+
+    def ready(self):
+        import peru_exp_app.signals
