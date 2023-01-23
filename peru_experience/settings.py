fd13aeaa3dcebbb55d4a657de25c60d42dc18fb2
IvetteMcDermott <ivettedemcdermott@gmail.com>
2023-01-23 14:20:48 +0000

    Set debug to False for update-deployment

--- peru_experience/settings.py
+++ peru_experience/settings.py
@@ -19,7 +19,6 @@ if os.path.isfile("env.py"):
     import env
 
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
@@ -32,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
