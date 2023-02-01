3e90e9ad76e01cc8bc09ca7793b477c60684fc89
IvetteMcDermott <ivettedemcdermott@gmail.com>
2023-02-01 17:41:07 +0000

    Uncommented code to allow app to run again
    Code was commented out for run migration and avoid conflicts
    Few changes in HTML as the comments still using place for the
    Place foreing Key

--- peru_exp_app/admin.py
+++ peru_exp_app/admin.py
@@ -1,26 +1,26 @@
 from django.contrib import admin
 from .models import Place, Comment, UserProfile
 
-# Register your models here.
+#Register your models here.
 
 
-# @admin.register(Place)
-# class PlacesItems(admin.ModelAdmin):
-#     prepopulated_fields = {'slug': ('name',)}
-#     list_filter = ('region', 'type_location')
-#     search_fields = ['region', 'type_location', 'name']
-#     list_display = ('name', 'type_location', 'cardinal_location', 'info', 'region', 'altitude', 'author', 'date_created', 'date_updated')
+@admin.register(Place)
+class PlacesItems(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    list_filter = ('region', 'type_location')
+    search_fields = ['region', 'type_location', 'name']
+    list_display = ('name', 'type_location', 'cardinal_location', 'info', 'region', 'altitude', 'author', 'date_created', 'date_updated')
 
 
-# @admin.register(Comment)
-# class CommentAdmin(admin.ModelAdmin):
-#     list_display = ('name', 'body', 'author', 'date_created', 'date_updated')
-#     list_filter = ('author', 'name')
-#     search_fields = ['author', 'name']
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('place', 'body', 'author', 'date_created', 'date_updated')
+    list_filter = ('author', 'place')
+    search_fields = ['author', 'place']
 
 
-# @admin.register(UserProfile)
-# class UserProfile(admin.ModelAdmin):
-#     list_display = ['user', 'user_name']
-#     list_filter = ('user_name', 'interests')
-#     search_fields = ['user_name']
+@admin.register(UserProfile)
+class UserProfile(admin.ModelAdmin):
+    list_display = ['user',]
+    list_filter = ('user', 'interests')
+    search_fields = ['user_name']
